5af9058a92ba0a80a67b25ac0104a4800c3de7e4
TheSal <visal012896@gmail.com>
2020-07-29 13:44:01 +0700

    push code, auth bloc

---
+++ lib/authentication_bloc/authentication_bloc.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_login_flutter/repositories/user_repository.dart';
+import 'package:flutter/cupertino.dart';
+
+part 'authentication_event.dart';
+
+part 'authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository _userRepository;
+
+  AuthenticationBloc({@required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(AuthenticationInitial());
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AuthenticationStarted) {
+      yield* _mapAuthenticationStartedToState();
+    } else if (event is AuthenticationLoggedIn) {
+      yield* _mapAuthenticationLoggedInToState();
+    } else if (event is AuthenticationLoggedOut) {
+      yield* _mapAuthenticationLoggedOutToState();
+    }
+  }
+
+  Stream<AuthenticationState> _mapAuthenticationStartedToState() async* {
+    final isSignedIn = await _userRepository.isSignedIn();
+    if (isSignedIn) {
+      final name = await _userRepository.getUser();
+      yield AuthenticationSuccess(name);
+    } else {
+      yield AuthenticationFailure();
+    }
+  }
+
+  Stream<AuthenticationState> _mapAuthenticationLoggedInToState() async* {
+    yield AuthenticationSuccess(await _userRepository.getUser());
+  }
+
+  Stream<AuthenticationState> _mapAuthenticationLoggedOutToState() async* {
+    yield AuthenticationFailure();
+    _userRepository.signOut();
+  }
+}
+
+/*
+echo "# flutter_bloc_login_firebase" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/VisalGGEZ/flutter_bloc_login_firebase.git
+git push -u origin master
+
+ */
